628271c3a4a8a1ef7cb8943899e3df5d0b258f06
Anders Børjesson <anbo@zealand.dk>
2020-09-09 10:36:35 +0200

    User controller

---
+++ bikefinder/Controllers/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bikefinder.Controllers
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string FirebaseUserId { get; set; }
+    }
+}
---
+++ bikefinder/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bikefinder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private static readonly List<User> Users = new List<User>();
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return Users;
+        }
+
+        // GET: api/User/5
+        [HttpGet("{firebaseUserId}", Name = "Get")]
+        public User Get(string firebaseUserId)
+        {
+            return Users.FirstOrDefault(user => user.FirebaseUserId == firebaseUserId);
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public void Post([FromBody] User newUser)
+        {
+            Users.Add(newUser);
+        }
+
+        // PUT: api/User/5
+        //[HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        // [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
